55e544f62b6415c4109b2b8494962fb8a72a3175
garden-lee <garden.lee@toss.im>
2022-06-04 16:31:10 +0900

    방위대 화이팅!

---
+++ src/main/kotlin/kr/mashup/bangwidae/asked/AskedApplication.kt
@@ -0,0 +1,11 @@
+package kr.mashup.bangwidae.asked
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AskedApplication
+
+fun main(args: Array<String>) {
+	runApplication<AskedApplication>(*args)
+}
---
+++ src/test/kotlin/kr/mashup/bangwidae/asked/AskedApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.mashup.bangwidae.asked
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AskedApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
