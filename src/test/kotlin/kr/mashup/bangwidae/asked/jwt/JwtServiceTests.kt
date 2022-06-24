a4e5225b87123c3afd3f4b08159c44ea8a55f9b3
alertjjm <sdr2936@gmail.com>
2022-06-25 00:48:26 +0900

    jwt test 추가

---
+++ src/test/kotlin/kr/mashup/bangwidae/asked/jwt/JwtServiceTests.kt
@@ -0,0 +1,29 @@
+package kr.mashup.bangwidae.asked.jwt
+
+import kr.mashup.bangwidae.asked.config.auth.jwt.JwtService
+import org.assertj.core.api.Assertions.assertThat
+import org.bson.types.ObjectId
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JwtServiceTests(
+	@Autowired
+	val jwtService: JwtService
+) {
+	@Test
+	@DisplayName("jwt encode & decode test")
+	fun jwtEncodeDecodeTest() {
+		//given
+		val oid = ObjectId().toHexString()
+
+		//when
+		val token = jwtService.createAccessToken(oid)
+		val decodedOid = jwtService.decodeToken(token)
+
+		//then
+		assertThat(oid).isEqualTo(decodedOid)
+	}
+}
