4e5a1dec1fd0afe8dd18ddbe996d2970db1795be
garden-lee <garden.lee@toss.im>
2022-06-25 15:36:08 +0900

    config 암호화

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ val javaJwtVersion: String by project
 val kotlinLoggingVersion: String by project
 val jacksonKotlinModuleVersion: String by project
 val embeddedMongoVersion: String by project
+val jasyptVersion: String by project
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
@@ -28,6 +29,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 
 	implementation("com.auth0:java-jwt:$javaJwtVersion")
+	implementation("com.github.ulisesbocchio:jasypt-spring-boot-starter:${jasyptVersion}")
 	implementation("io.github.microutils:kotlin-logging-jvm:${kotlinLoggingVersion}")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
---
+++ src/main/kotlin/kr/mashup/bangwidae/asked/config/encrypt/EncryptConfig.kt
@@ -0,0 +1,9 @@
+package kr.mashup.bangwidae.asked.config.encrypt
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableEncryptableProperties
+class EncryptConfig {
+}
