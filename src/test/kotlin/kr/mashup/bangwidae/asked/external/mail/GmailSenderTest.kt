916cbafaad058ce9792af82e95dbbaeae656bfeb
garden-lee <garden.lee@toss.im>
2022-06-27 20:04:22 +0900

    메일 발송 테스트 disabled

--- src/test/kotlin/kr/mashup/bangwidae/asked/external/mail/GmailSenderTest.kt
+++ src/test/kotlin/kr/mashup/bangwidae/asked/external/mail/GmailSenderTest.kt
@@ -1,10 +1,11 @@
 package kr.mashup.bangwidae.asked.external.mail
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
+@Disabled("메일 보내보고 싶을 때 사용 해보기")
 @SpringBootTest
 class GmailSenderTest(
     @Autowired
@@ -13,7 +14,10 @@ class GmailSenderTest(
 
     @Test
     fun test() {
-        gmailSender.send()
+        val targetMail = "khjvvv7@gmail.com"
+        val title = "제목"
+        val text = "내용"
+        gmailSender.send(targetMail, title, text)
     }
 
 }
