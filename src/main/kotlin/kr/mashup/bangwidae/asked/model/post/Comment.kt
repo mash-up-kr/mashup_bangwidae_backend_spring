0b815f91f4200c6e64b7e92d88883d5d06dec379
alertjjm <sdr2936@gmail.com>
2022-07-17 01:01:39 +0900

    add @Id

--- src/main/kotlin/kr/mashup/bangwidae/asked/model/post/Comment.kt
+++ src/main/kotlin/kr/mashup/bangwidae/asked/model/post/Comment.kt
@@ -5,6 +5,7 @@ import kr.mashup.bangwidae.asked.model.Region
 import kr.mashup.bangwidae.asked.utils.GeoUtils
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.Id
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.annotation.Version
 import org.springframework.data.mongodb.core.geo.GeoJsonPoint
@@ -13,6 +14,7 @@ import java.time.LocalDateTime
 
 @Document("comment")
 data class Comment(
+    @Id
     val id: ObjectId? = null,
     val userId: ObjectId,
     val postId: ObjectId,
