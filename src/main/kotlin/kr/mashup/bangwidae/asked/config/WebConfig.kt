5863c3508089d0591e08909c2ba344c190ca6c11
alertjjm <sdr2936@gmail.com>
2022-07-19 22:36:10 +0900

    add web config

---
+++ src/main/kotlin/kr/mashup/bangwidae/asked/config/WebConfig.kt
@@ -0,0 +1,16 @@
+package kr.mashup.bangwidae.asked.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**").allowedMethods("*")
+            .allowedOrigins("http://localhost:3000")
+            .allowCredentials(true)
+    }
+}
