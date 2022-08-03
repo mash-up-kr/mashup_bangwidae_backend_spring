67c65a4beda5b48f109d0c5dfff1656464140447
SeongYunKim <cauboy17@naver.com>
2022-08-04 03:12:38 +0900

    사용자 링크 공유 API

--- src/main/kotlin/kr/mashup/bangwidae/asked/controller/dto/UserDto.kt
+++ src/main/kotlin/kr/mashup/bangwidae/asked/controller/dto/UserDto.kt
@@ -1,6 +1,8 @@
 package kr.mashup.bangwidae.asked.controller.dto
 
 import kr.mashup.bangwidae.asked.model.User
+import kr.mashup.bangwidae.asked.model.User.Companion.DEFAULT_PROFILE_IMAGE_URL
+import kr.mashup.bangwidae.asked.service.question.QuestionDomain
 
 data class UserInfoDto(
     val userId: String,
@@ -17,6 +19,54 @@ data class UserInfoDto(
                 tags = user.tags,
             )
         }
+    }
+}
+
+data class UserLinkShareInfoDto(
+    val user: UserInfoDto,
+    val representativeWardName: String,
+    val questions: List<QuestionAndAnswerDto>,
+) {
+    data class UserInfoDto(
+        val id: String,
+        val nickname: String,
+        val profileDescription: String,
+        val tags: List<String>,
+        val profileImageUrl: String,
+        val level: Int,
+    ) {
+        companion object {
+            fun from(user: User) = UserInfoDto(
+                id = user.id!!.toHexString(),
+                nickname = user.nickname!!,
+                profileDescription = user.description ?: "",
+                tags = user.tags,
+                profileImageUrl = user.profileImageUrl ?: DEFAULT_PROFILE_IMAGE_URL,
+                level = user.level,
+            )
+        }
+    }
+
+    data class QuestionAndAnswerDto(
+        val questionId: String,
+        val questionContent: String,
+        val answerContent: String,
+    ) {
+        companion object {
+            fun from(question: QuestionDomain) = QuestionAndAnswerDto(
+                questionId = question.id,
+                questionContent = question.content,
+                answerContent = question.answer!!.content,
+            )
+        }
+    }
+
+    companion object {
+        fun from(user: User, questions: List<QuestionDomain>) = UserLinkShareInfoDto(
+            user = UserInfoDto.from(user),
+            representativeWardName = "todo - 우리집",
+            questions = questions.map { QuestionAndAnswerDto.from(it) },
+        )
     }
 }
 
