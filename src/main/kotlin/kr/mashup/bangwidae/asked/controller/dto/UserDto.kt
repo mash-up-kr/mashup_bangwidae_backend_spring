443fb4f681b93c302d05dd9c796051f341179e8f
gardenlee <garden.lee@toss.im>
2022-08-09 20:28:55 +0900

    user info 필드 nullable하게 변경

--- src/main/kotlin/kr/mashup/bangwidae/asked/controller/dto/UserDto.kt
+++ src/main/kotlin/kr/mashup/bangwidae/asked/controller/dto/UserDto.kt
@@ -4,16 +4,16 @@ import kr.mashup.bangwidae.asked.model.User
 
 data class UserInfoDto(
     val userId: String,
-    val nickname: String,
-    val profileDescription: String,
+    val nickname: String?,
+    val profileDescription: String?,
     val tags: List<String>,
 ) {
     companion object {
         fun from(user: User): UserInfoDto {
             return UserInfoDto(
                 userId = user.id!!.toHexString(),
-                nickname = user.nickname!!,
-                profileDescription = user.description!!,
+                nickname = user.nickname,
+                profileDescription = user.description,
                 tags = user.tags,
             )
         }
