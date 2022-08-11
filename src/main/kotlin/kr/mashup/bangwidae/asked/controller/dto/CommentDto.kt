b400e48854480f420a7fd1a9872f73df6e99b514
alertjjm <sdr2936@gmail.com>
2022-08-11 22:40:24 +0900

    익명 추가

--- src/main/kotlin/kr/mashup/bangwidae/asked/controller/dto/CommentDto.kt
+++ src/main/kotlin/kr/mashup/bangwidae/asked/controller/dto/CommentDto.kt
@@ -13,14 +13,16 @@ data class CommentWriteRequest(
     @ApiModelProperty(value = "경도", example = "136.4")
     val longitude: Double,
     @ApiModelProperty(value = "위도", example = "36.4")
-    val latitude: Double
+    val latitude: Double,
+    val anonymous: Boolean?
 ) {
     fun toEntity(userId: ObjectId, postId: ObjectId): Comment {
         return Comment(
             content = content,
             userId = userId,
             postId = postId,
-            location = GeoUtils.geoJsonPoint(longitude, latitude)
+            location = GeoUtils.geoJsonPoint(longitude, latitude),
+            anonymous = anonymous ?: false
         )
     }
 }
@@ -30,6 +32,7 @@ data class CommentEditRequest(
     val content: String,
     val longitude: Double?,
     val latitude: Double?,
+    val anonymous: Boolean?
 )
 
 data class CommentDto(
@@ -45,6 +48,8 @@ data class CommentDto(
     val userLiked: Boolean,
     @ApiModelProperty(value = "대표 주소", example = "분당구")
     val representativeAddress: String?,
+    @ApiModelProperty(value = "익명 여부", example = "true")
+    val anonymous: Boolean,
     @ApiModelProperty(value = "생성일", example = "2022-07-14T23:57:33.436")
     val createdAt: LocalDateTime?,
     @ApiModelProperty(value = "수정일", example = "2022-07-14T23:57:33.436")
@@ -54,15 +59,12 @@ data class CommentDto(
         fun from(user: User, comment: Comment, likeCount: Int, userLiked: Boolean): CommentDto {
             return CommentDto(
                 id = comment.id!!.toHexString(),
-                user = CommentWriter(
-                    id = user.id!!.toHexString(),
-                    tags = user.tags,
-                    nickname = user.nickname!!
-                ),
+                user = comment.getWriter(user),
                 content = comment.content,
                 likeCount = likeCount,
                 userLiked = userLiked,
                 representativeAddress = comment.region?.representativeAddress,
+                anonymous = comment.anonymous ?: false,
                 createdAt = comment.createdAt,
                 updatedAt = comment.updatedAt
             )
@@ -88,11 +90,7 @@ data class CommentResultDto(
         fun from(user: User, comment: Comment): CommentResultDto {
             return CommentResultDto(
                 id = comment.id!!.toHexString(),
-                user = CommentWriter(
-                    id = user.id!!.toHexString(),
-                    tags = user.tags,
-                    nickname = user.nickname!!
-                ),
+                user = comment.getWriter(user),
                 content = comment.content,
                 representativeAddress = comment.region?.representativeAddress,
                 createdAt = comment.createdAt,
@@ -108,5 +106,18 @@ data class CommentWriter(
     @ApiModelProperty(value = "User 태그리스트", example = "[MBTI, 산책]")
     val tags: List<String> = emptyList(),
     @ApiModelProperty(value = "User 닉네임", example = "noname")
-    val nickname: String
-)
+    val nickname: String,
+    @ApiModelProperty(value = "글 작성자 프로필 이미지", example = "http://image.com")
+    val profileImageUrl: String?
+) {
+    companion object {
+        fun from(user: User) : CommentWriter {
+            return CommentWriter(
+                id = user.id!!.toHexString(),
+                tags = user.tags,
+                nickname = user.nickname!!,
+                profileImageUrl = user.profileImageUrl
+            )
+        }
+    }
+}
