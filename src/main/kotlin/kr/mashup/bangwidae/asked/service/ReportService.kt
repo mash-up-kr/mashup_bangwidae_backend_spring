72b8aa06a760268f030833f7ae61218a1888a751
alertjjm <sdr2936@gmail.com>
2022-08-22 00:07:05 +0900

    신고하기 api 수정

--- src/main/kotlin/kr/mashup/bangwidae/asked/service/ReportService.kt
+++ src/main/kotlin/kr/mashup/bangwidae/asked/service/ReportService.kt
@@ -1,14 +1,17 @@
 package kr.mashup.bangwidae.asked.service
 
 import kr.mashup.bangwidae.asked.model.document.Report
+import kr.mashup.bangwidae.asked.model.document.ReportType
+import kr.mashup.bangwidae.asked.model.document.User
 import kr.mashup.bangwidae.asked.repository.ReportRepository
+import org.bson.types.ObjectId
 import org.springframework.stereotype.Service
 
 @Service
 class ReportService(
     private val reportRepository: ReportRepository
 ) {
-    fun report(report: Report) {
-        reportRepository.save(report)
+    fun report(reporter: User, type: ReportType, targetId: ObjectId) {
+        reportRepository.save(Report(reporterUserId = reporter.id!!, type = type, targetId = targetId))
     }
 }
