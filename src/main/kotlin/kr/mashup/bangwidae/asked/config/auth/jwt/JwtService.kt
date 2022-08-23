9fc57ba9a8778003da94c8db78040e28f7dd9d68
alertjjm <sdr2936@gmail.com>
2022-08-23 22:29:40 +0900

    토큰 만료시간 3분으로 수정

--- src/main/kotlin/kr/mashup/bangwidae/asked/config/auth/jwt/JwtService.kt
+++ src/main/kotlin/kr/mashup/bangwidae/asked/config/auth/jwt/JwtService.kt
@@ -49,7 +49,7 @@ class JwtService(
     private fun accessTokenExpired(): Date {
         val cal = Calendar.getInstance()
         cal.time = Date()
-        cal.add(Calendar.HOUR, 1)
+        cal.add(Calendar.MINUTE, 3)
         return cal.time
     }
 
