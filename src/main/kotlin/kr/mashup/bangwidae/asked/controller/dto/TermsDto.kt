4db10cefbbb09380a6cf3ba7308b2c16fe65b668
정종민[Ad Service Dev1] <jongmin.jung@navercorp.com>
2022-08-26 17:06:04 +0900

    terms 에 createdAt, updatedAt 추가

--- src/main/kotlin/kr/mashup/bangwidae/asked/controller/dto/TermsDto.kt
+++ src/main/kotlin/kr/mashup/bangwidae/asked/controller/dto/TermsDto.kt
@@ -2,12 +2,15 @@ package kr.mashup.bangwidae.asked.controller.dto
 
 import kr.mashup.bangwidae.asked.model.document.terms.Terms
 import org.bson.types.ObjectId
+import java.time.LocalDateTime
 
 data class TermsDto(
     val id: ObjectId,
     val title: String,
     val content: String,
     val necessary: Boolean,
+    val createdAt: LocalDateTime,
+    val updatedAt: LocalDateTime,
 ) {
     companion object {
         fun from(terms: Terms): TermsDto {
@@ -16,6 +19,8 @@ data class TermsDto(
                 title = terms.title,
                 content = terms.content,
                 necessary = terms.isNecessary,
+                createdAt = terms.createdAt!!,
+                updatedAt = terms.updatedAt!!,
             )
         }
     }
